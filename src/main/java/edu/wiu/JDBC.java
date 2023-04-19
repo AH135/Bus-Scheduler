00389d88610e1e80927d0dd0d56c843eeb996f52
AH135 <57057240+AH135@users.noreply.github.com>
2023-04-19 13:58:05 -0500

    added error checking method, and switch tree for user inputs

--- src/main/java/edu/wiu/JDBC.java
+++ src/main/java/edu/wiu/JDBC.java
@@ -30,11 +30,66 @@ public class JDBC {
             conn.setAutoCommit(false);
             stmt = conn.createStatement();
 
-            ResultSet rset = stmt.executeQuery("Select Example" + "stuff");
 
-            while (rset.next()){
-                System.out.println(rset.getString(1));
+
+
+            //code here for program flow
+            //condition for loop end
+            boolean program_end = false;
+            //while program_end is = false, loop will continue
+            while(program_end == false) {
+                //create arraylist for invalid strings
+                ArrayList<String> valid_inputs1 = new ArrayList<String>();
+                valid_inputs1.add("view");
+                valid_inputs1.add("update");
+                valid_inputs1.add("insert");
+                Boolean input_check = false;
+
+                //-ask user for a new entry or an update to an existing entry
+                String input = null;
+                while (input_check == false) {
+                    System.out.println("Welcome. What would you like to do? [view] tables, [update] tables, or [insert] new entries?");
+
+                    input = keyboard.readLine();
+
+                    //check for errors
+                    input_check = check_string_errors(input, valid_inputs1);
+                    //System.out.println(input_check);
+                }
+
+                //switch branch for different options
+                switch (input) {
+                    //this needs to show view options and then print them
+                    case ("view"):
+                        break;
+                    //this needs to allow user to update an existing table
+                    case("update"):
+                        break;
+                    //this needs to allow the user to insert into an existing table
+                    case("insert"):
+                        break;
+                }
+
+                //current breakpoint, for debugging currently
+                break;
+
+
             }
+
+            /*demo for user prompts
+            //Scanner scan = new Scanner(System.in);
+            String input = keyboard.readLine();
+            System.out.println(input);*/
+
+            //this is the table object created from a select statement
+            //ResultSet rset = stmt.executeQuery("Select Example" + "stuff");
+            ResultSet rset = stmt.executeQuery("Select * from Doctors");
+
+            //example of printed table, columns concatenated together while it loops.
+            while (rset.next()){
+                System.out.println(rset.getString(1) +" "+  rset.getString(2));
+            }
+
             rset.close();
 
 
@@ -43,6 +98,26 @@ public class JDBC {
 
         }
 
+
+    }
+
+    //method that takes in a string and an arraylist, returns a fail if the "original"
+    //string is not within the arraylist
+    public static boolean check_string_errors(String original, ArrayList<String> check){
+        //boolean check_fail = false;
+        //System.out.println(original);
+        //System.out.println(check.toString());
+
+        for(int i = 0; i < check.size(); i++){
+            //System.out.println(check.get(i));
+            if(original.equals(check.get(i))){
+                System.out.println("check passes");
+                return true;
+                //break;
+            }
+        }
+        System.out.println("check fail");
+        return false;
 
     }
 
